4f85e1d9f68ae80cde54e3ed72eb48332f3cb210
hoseinparyab <75984205+hoseinparyab@users.noreply.github.com>
2024-12-10 22:15:18 +0330

    feat: Add home page layering and assets
    - Implemented initial home page structure with proper layering.
    - Added necessary assets (images, styles, scripts) for the home page.
    - Optimized asset organization for better scalability.

--- Modules/PYB/Home/Providers/HomeServiceProvider.php
+++ Modules/PYB/Home/Providers/HomeServiceProvider.php
@@ -9,7 +9,8 @@ class HomeServiceProvider  extends ServiceProvider
 {
     public function register ()
     {
-
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Home');
+        //        $this->loadRoutesFrom(__DIR__ . '/../Routes/home_routes.php');
         Route::middleware('web')->namespace('PYB\Home\Http\Controllers')->group(__DIR__ . '/../Routes/home_routes.php');
     }
 
