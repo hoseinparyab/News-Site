cb485560b1ed05ba9cc201a1bcba3e904a23e532
hoseinparyab <75984205+hoseinparyab@users.noreply.github.com>
2024-12-19 01:56:20 +0330

    feat(users): launch the users module
    - Add routes, controllers, and views for the Users module
    - Implement user listing, creation, editing, and deletion
    - Integrate validation and error handling for user management

--- Modules/PYB/User/Providers/UserServiceProvider.php
+++ Modules/PYB/User/Providers/UserServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace PYB\User\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
@@ -8,5 +9,18 @@ class UserServiceProvider extends ServiceProvider
     public function register()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'User');
+        Route::middleware('web')->namespace('PYB\User\Http\Controllers')->group(__DIR__ . '/../Routes/user_routes.php');
     }
+    public function boot()
+    {
+        $this->app->booted(static function () {
+            config()->set('panelConfig.menus.users', [
+                'url'   => route('users.index'),
+                'title' => 'کاربران ',
+                'icon'  => 'account',
+            ]);
+        });
+    }
+
 }
